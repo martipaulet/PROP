525889db2631c12097017fddcc191a5695dfdc54
joelf <joel.fernandez.abad@estudiantat.upc.edu>
2022-10-23 18:09:41 +0200

    Eliminada clase titol i pasa a ser atribut string de document
    De la classe autor he fet la funció de ConsultaTítols i d'obtéDocument, encara que aquesta última no se si finalment caldrà o es podrà fer mitjançant el controlador
    De la classe Document, a causa de les funcions fetes a Autor he fet funcions que retornen títol, una que retorna autor i una de mostra document però aquesta no he posat codi
    En procés de fer OrdreAlfabetic

--- Document.java
+++ Document.java
@@ -1,20 +1,27 @@
 import java.util.*;
 
 public class Document {
+    private String titol;
     private Date data_creacio;
     private Date data_modificacio;
     private String contingut;
     private HashMap<String, Integer> paraules;
 
-    private Autor autor_doc;
-    private Titol titol_doc;
+    private String autor_doc;
     private Vector<Frase> frases;
     //private ConjuntDocuments DocumentsBD;
 
-    public Document (Autor a, Titol t, String c) {
+    public Document (Autor a, /*titol t,*/ String c) {
         //creadora d'un nou document
     }
 
+    public String getTitol() {
+        return titol;
+    }
+
+    public String getAutor() {
+        return autor_doc;
+    }
     public void modificaDocument(/* paràmetres*/) {
         //modifica els paràmetres que seleccioni i passi com paràmetre
     }
@@ -26,5 +33,8 @@ public class Document {
     public void llistaDocuments() {
         //llista documents del conjunt segons criteri ordenacio
         //trucar al criteri d'ordenacio
+    }
+
+    public void mostraDocument() {
     }
 }
--- OrdreAlfabetic.java
+++ OrdreAlfabetic.java
@@ -2,6 +2,24 @@ import java.util.*;
 
  class OrdreAlfabetic implements Ordenacio {
 
-    public void OrdenarDocuments(Vector<Document> vd) {}
-    //ordenar per ordre alfabètic títol
+     //ordenar per ordre alfabètic títol
+    public void OrdenarDocuments(Vector<Document> vd) {
+       /* HashMap<HashMap<String, String>, Document> docs = new HashMap<>();
+        for (int i = 0; i < vd.size(); ++i) {
+            Document d = vd.elementAt(i);
+            String tit = d.getTitol();
+            String a = d.getAutor();
+            docs.put(docs.put(tit, a), d);
+
+        } */
+        Vector<Document> vdReal = new Vector<>();
+        for (int i = 0; i < vd.size(); ++i) {
+            Document d = vd.elementAt(i);
+            String tit_doc = d.getTitol();
+            /*INSERTAR EN VECTOR EL DOCUMENTO Y HACER ORDENACIÓN POR INSERCIÓN COMPARANDO TÍTULOS
+              SI TÍTULOS FUERAN IGUALES, PASAMOS A COMPARAR AUTOR
+             */
+        }
+    }
+
 }
