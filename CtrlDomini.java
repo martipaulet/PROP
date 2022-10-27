e94bbd0bda4cf759db4c20394a429911050bb493
Martí Paulet López <marti.paulet@estudiantat.upc.edu>
2022-10-27 11:55:39 +0200

    ultima versio ctrl domini

--- CtrlDomini.java
+++ CtrlDomini.java
@@ -52,9 +52,7 @@ public class CtrlDomini {
     //      Si l'autor del document eliminat nomes tenia aquest document tambe s'eimina l'autor
 
     public void baixaDocument(Document d) {
-        if (desassociaAutor(d)) {
-            autors.remove(d.getAutor());
-        }
+        desassociaAutor(d);
         documents.baixaDocument(d);
     }
 
@@ -66,54 +64,22 @@ public class CtrlDomini {
 
 
 
-    //EXC: Si la parella nouAutor, nouTitol ja existia es rebutja la modificació.
-    //post: Actualitzar autor al document modificat. Si no existia es crea el nouAutor.
-    //          -> 1) AutorAntic amb més documents: dessasociar document amb l'AutorAntic i fer nova associació
-    //          -> 2) AutorAntic amb únic document: l'AutorAntic passa a no tenir documents -> eliminar autor
-    //      Actualitzar titol al document modificat.
-    //      Actualitzar contingut al document modificat.
-    //      Actualitzar dataUltimaModificacio al document modificat.
-    //      Actualitzar setFrases i setParaules al document modificat
-
-    public void modificarDocument(String nouAutor, String nouTitol, String nouContingut, Document d, Date novaData) {
-        boolean canviAutor = d.getAutor().equals(nouAutor);
-        boolean canviTitol = d.getTitol().equals(nouTitol);
-
-        //Cambien els 2: comprobar que no existeix el nou document(nouAutor,nouTitol)
-        //Cambia nomes titol: comprobar que l'autor no tenia ja un document amb aquest nouTitol
-        //cambia nomes autor: comprobar que nouAutor no tenia ja un document amb aquell titol
-
-        if (canviAutor && canviTitol) {
-            if (!existeixDocument(nouAutor,nouTitol)) {
-                //eliminar associacio anticAutor amb anticDocument
-                //si anticAutor nomes tenia anticDocument eliminar l'autor
-                if (desassociaAutor(d)) {
-                    autors.remove(d.getAutor());
-                }
-
-            }
+    //pre: nomes es pot modificar el contingut d'un document
+    //post: S'actualizta el contingut i la data de ultima modificació del document si es que s'ha modificat
+    public void modificarDocument(String nouContingut, Document d, Date novaData) {
+        if (!d.getContingut().equals(nouContingut)) {
+            d.actualitzaDocument(nouContingut, novaData);
         }
-
-            if (!existeixAutor(nouAutor)) {
-                Autor a = new Autor(nouAutor);
-            }
-            Autor a = autors.get(nouAutor);
-
-            if (desassociaAutor(d)) {
-                autors.remove(d.getAutor());
-            }
-
-            d.actualitzaDocument(nouAutor, novaData);
-        }
+    }
 
 
     //post: elimina l'associacio entre el document d i el seu autor.
     //      si l'autor nomes tenia aquell document retorna true. Altrament retorna false
 
-    public boolean desassociaAutor(Document d) {
-
-        return true;
-
+    public void desassociaAutor(Document d) {
+        Autor tmp = autors.get(d.getAutor());
+        tmp.eliminaDocument(d);
+        if(!tmp.teDocuments()) autors.remove(d.getAutor());
     }
 
 
