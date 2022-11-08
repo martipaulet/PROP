58017bc0348ef5fc4c9f141bb404f98c8d2db94b
albert.cardona.zaera <albert.cardona.zaera@estudiantat.upc.edu>
2022-11-08 18:00:16 +0100

    He afegit un atribut (RecParaules) a Frase, que compta els cops que surt la paraula en la frase

--- Domini/Frase.java
+++ Domini/Frase.java
@@ -4,8 +4,23 @@ import java.util.*;
 
 public class Frase {
     private String[] paraules_;
+    private HashMap<String, Integer> RecParaules = new HashMap<>();
 
     public Frase(String paraules) {
         paraules_ = paraules.split("\\W+");
     }
+
+    private void setParaules() {
+        for (String paraula : paraules_) {
+            paraula = paraula.toLowerCase();
+            if (RecParaules.containsKey(paraula)) {
+                int vegades = RecParaules.get(paraula);
+                RecParaules.put(paraula, ++vegades);
+            }
+            else {
+                RecParaules.put(paraula, 1);
+            }
+        }
+    }
+
 }
