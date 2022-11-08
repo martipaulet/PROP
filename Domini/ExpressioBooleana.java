dcb7bc798d3bf9c7519f7e05c62a182db209c8c3
joelf <joel.fernandez.abad@estudiantat.upc.edu>
2022-11-08 16:42:38 +0100

    Classe ExpressioBooleana

---
+++ Domini/ExpressioBooleana.java
@@ -0,0 +1,17 @@
+package Domini;
+import java.util.*;
+
+public class ExpressioBooleana {
+    private String query;
+    private ExpressionTree expressio;
+
+    public ExpressioBooleana(String e) {
+        query = e;
+        expressio = new ExpressionTree(e);
+    }
+
+    public void modificaExpressioBooleana(String e) {
+        query = e;
+        expressio.modifica(e);
+    }
+}
