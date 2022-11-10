7b16047ae80911468cb6c7e4c1368c07d2c881bf
Oriol Ologaray <oriol.ologaray@estudiantat.upc.edu>
2022-11-10 13:20:03 +0100

    Test Document i menu main

--- Main.java
+++ Main.java
@@ -1,64 +1,77 @@
 import Domini.*;
+import CtrlDomini.CtrlDomini;
+
+import javax.swing.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Autor a = new Autor("George R. Martin");
-        String c = "Aixo es: un exemple, de contingut. Es un exemple amb paraules repetides";
-        Document d = new Document(a.getNom(), "Juego de tronos", c);
-        d.imprimir();
+        boolean exit = false;
+        String demanaAutor = "Introdueix un autor:\n";
+        String demanaTitol = "Introdueix un títol:\n";
+        String demanaContingut = "Introdueix el contingut:\n";
+        String autor;
+        String titol;
+        String contingut;
+        CtrlDomini cd = CtrlDomini.getInstance();
 
-        String c1 = "a";
-        String c2 = "b";
-        String c3 = "c";
-        String c4 = "d";
-
-        Document d1 = new Document(a.getNom(), "Hola", c1);
-        Document d2 = new Document(a.getNom(), "Blau", c2);
-        //Document d3 = new Document(a.getNom(), "Hola", c3);
-        Document d4 = new Document(a.getNom(), "Adeu", c4);
-
-        a.afegirDocument(d1);
-        a.afegirDocument(d2);
-        a.afegirDocument(d4);
-        a.afegirDocument(d);
-
-        /*Document res = a.obteDocument("Blau");
-        if (res == d2) System.out.println("Funciona");
-        Document res2 = a.obteDocument("Adeu");
-        if (res2 != d2) System.out.println("funciona"); */
-
-        //DOC 1
-        Autor a1 = new Autor ("Juan");
-        Autor a2 = new Autor ("Pedro");
-        Autor a3 = new Autor ("Mireia");
-        Autor a4 = new Autor ("Angel");
-
-        Boolean b1 = a1.tePrefix("Ju");
-        Boolean b2 = a2.tePrefix("");
-        Boolean b3 = a3.tePrefix("Mireiax");
-        Boolean b4 = a4.tePrefix("Aj");
-
-        if (b1) System.out.println("b1 true");
-        if (b2) System.out.println("b2 true");
-        if (b3) System.out.println("b3 true");
-        if (b4) System.out.println("b4 true");
-
-       /* Vector<Document> vd = new Vector<>();
-        vd.add(d1);
-        vd.add(d2);
-        vd.add(d3);
-        vd.add(d4);
-
-        Vector<Document> vdReal = new Vector<>();
-        OrdreCreacio o = new OrdreCreacio();
-        vdReal = o.OrdenarDocuments(vd);
-        for (int i = 0; i < vdReal.size(); ++i) {
-            Document doc = vdReal.elementAt(i);
-            String s =doc.getAutor();
-            System.out.println(s);
+        for (int i = 0; i < 4; ++i) {
+            Autor a = new Autor("Aa"+i);
+            System.out.println(a.getNom());
         }
-        System.out.println('\n');*/
+        new Autor("Ab");
+        while (!exit) {
+            String menu = "-- MENU --\n"+
+                    "1. Alta Document\n"+
+                    "2. Baixa Document\n"+
+                    "3. Modificar Document\n"+
+                    "4. Llista de títols autor\n"+
+                    "5. Llista d'autors per prefix\n"+
+                    "6. Obtenir contingut d'un document\n"+
+                    "7. Llistar documents semblants\n"+
+                    "8. Llistar documents que compleixen una expressió\n"+
+                    "9. Sortir\n";
+            int opcio = Integer.parseInt(JOptionPane.showInputDialog(menu));
+            switch (opcio) {
+                case 1:
+                    autor = JOptionPane.showInputDialog(demanaAutor);
+                    titol = JOptionPane.showInputDialog(demanaTitol);
+                    contingut = JOptionPane.showInputDialog(demanaContingut);
+                    cd.altaDocument(autor, titol, contingut);
+                    break;
+                case 2:
+                    autor = JOptionPane.showInputDialog(demanaAutor);
+                    titol = JOptionPane.showInputDialog(demanaTitol);
+                    cd.baixaDocument(autor, titol);
+                    break;
+                case 3:
+                    autor = JOptionPane.showInputDialog(demanaAutor);
+                    titol = JOptionPane.showInputDialog(demanaTitol);
+                    contingut = JOptionPane.showInputDialog(demanaContingut);
+                    cd.modificarDocument(contingut, autor, titol);
+                    break;
+                case 4:
+                    autor = JOptionPane.showInputDialog(demanaAutor);
+                    cd.titolsAutor(autor);
+                    break;
+                case 5:
+                    String prefix = JOptionPane.showInputDialog("Introdueix un prefix:\n");
+                    ArrayList<String> autors = cd.prefixAutor(prefix);
+                    for (String a : autors) {
+                        JOptionPane.showMessageDialog(null, a);
+                    }
+                    break;
+                case 6:
+                    autor = JOptionPane.showInputDialog(demanaAutor);
+                    titol = JOptionPane.showInputDialog(demanaTitol);
+                    JOptionPane.showMessageDialog(null, cd.obteContingut(autor, titol));
+                case 9:
+                    exit = true;
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + opcio);
+            }
+        }
 
     }
 }
