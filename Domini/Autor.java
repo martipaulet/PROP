23725ff625a05cf735cef2e8b554f73143bd1d47
Oriol Ologaray <oriol.ologaray@estudiantat.upc.edu>
2022-11-13 23:34:20 +0100

    Update autor

--- Domini/Autor.java
+++ Domini/Autor.java
@@ -7,8 +7,8 @@ public class Autor {
     //---ATRIBUTS---
 
 
-    private String nomA; //nom de l'autor.
-    private Vector<Document> docsAutor_; //Conjunt de documents de l'autor.
+    private String nom_; //nom de l'autor.
+    private Vector<Document> docs_; //Conjunt de documents de l'autor.
 
 
     //---CONSTRUCTORES---
@@ -16,14 +16,14 @@ public class Autor {
 
     //Post: es crea una instancia d'Autor buida.
     public Autor() {
-        nomA = null;
-        docsAutor_ = new Vector<>();
+        nom_ = null;
+        docs_ = new Vector<>();
     }
 
     //Post: es crea una instancia d'Autor amb nomA = nomAutor.
     public Autor(String nomAutor) {
-        nomA = nomAutor;
-        docsAutor_ = new Vector<>();
+        nom_ = nomAutor;
+        docs_ = new Vector<>();
     }
 
 
@@ -32,12 +32,12 @@ public class Autor {
 
     //Post: retorna el nom de l'autor.
     public String getNom() {
-        return nomA;
+        return nom_;
     }
 
     //Post: s'assigna al autor el nomA = nomAutor.
     public void setNom(String nomAutor) {
-        nomA = nomAutor;
+        nom_ = nomAutor;
     }
 
 
@@ -47,8 +47,8 @@ public class Autor {
     //Post: es retorna una llista dels titols de l'autor ordenats alfabèticament.
     public ArrayList<String> consultaTitols() {
         ArrayList<String> t = new ArrayList<>();
-        for (int i = 0; i < docsAutor_.size(); ++i) {
-            Document d = docsAutor_.elementAt(i);
+        for (int i = 0; i < docs_.size(); ++i) {
+            Document d = docs_.elementAt(i);
             String titolAutor = d.getTitol();
             t.add(titolAutor);
         }
@@ -60,8 +60,8 @@ public class Autor {
     public boolean conteTitol(String titol) {
         boolean trobat = false;
         int i = 0;
-        while ( i < docsAutor_.size() && !trobat) {
-            Document d = docsAutor_.elementAt(i);
+        while ( i < docs_.size() && !trobat) {
+            Document d = docs_.elementAt(i);
             if (d.getTitol().equals(titol)) trobat = true;
             ++i;
         }
@@ -71,27 +71,27 @@ public class Autor {
     //Pre: L'autor del document es this.nomA.
     //Post: S'afegeix al conjunt de documents de l'autor el document d.
     public void afegirDocument(Document d) {
-        docsAutor_.add(d);
+        docs_.add(d);
     }
 
     //Pre: L'autor del document es this.nomA.
     //Post: S'elimina del conjunt de documents de l'autor el document d.
     public void eliminaDocument(Document d) {
-        docsAutor_.remove(d);
+        docs_.remove(d);
     }
 
     //Post: retorna true si el nom de l'autor comença pel prefix indicat. False altrament.
     public boolean tePrefix(String prefix) {
-        boolean trobat = prefix.length() <= nomA.length();
+        boolean trobat = prefix.length() <= nom_.length();
         for (int i = 0; i < prefix.length() && trobat; ++i) {
-            if (prefix.charAt(i) != nomA.charAt(i)) trobat = false;
+            if (prefix.charAt(i) != nom_.charAt(i)) trobat = false;
         }
         return trobat;
     }
 
     //Post: retorna true si l'autor te documents. False altrament.
     public boolean teDocuments() {
-        return (docsAutor_.size() != 0);
+        return (docs_.size() != 0);
     }
 
 }
