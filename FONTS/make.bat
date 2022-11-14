409a085a8abd6ca45afa3e925a379f617208ef15
Oriol Ologaray <oriol.ologaray@estudiantat.upc.edu>
2022-11-14 23:32:28 +0100

    FUNCIONA

--- FONTS/make.bat
+++ FONTS/make.bat
@@ -2,8 +2,10 @@ javac Domini/*.java
 javac Domini/*.java Controladors/*.java
 javac -cp lib/junit-4.13.1.jar Domini/*.java JUnit/*.java
 javac Domini/*.java Controladors/*.java Drivers/*.java
-jar cvfe DriverCtrlDomini.jar Drivers/DriverCtrlDomini Controladors/*.class Domini/*.class Drivers/DriverCtrlDomini.class
+jar cvfe DriverCtrlDomini.jar Drivers/DriverCtrlDomini Domini/*.class Controladors/*.class Drivers/DriverCtrlDomini.class
 move Domini\*.class ..\EXE\CLASS\
 move Controladors\*class ..\EXE\CLASS\
-move Drivers\*.class .
+move Drivers\*.class ..\EXE\CLASS\
+move JUnit\*.class ..\EXE\CLASS\
+move DriverCtrlDomini.jar ..\EXE\
 pause
