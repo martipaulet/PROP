c047dab63b00935aa92277886a743332a0c6406e
albert.cardona.zaera <albert.cardona.zaera@estudiantat.upc.edu>
2022-11-14 20:10:27 +0100

    make.bat

---
+++ FONTS/make.bat
@@ -0,0 +1,8 @@
+javac --release 11 Controladors/*.java
+javac --release 11 Domini/*.java
+javac --release 11 Drivers/*.java
+javac --release 11 JUnit/*.java
+move Controladors/*.class ..\EXE\CLASS\Controladors
+move Domini/*.class ..\EXE\CLASS\Domini
+move Drivers/*.class ..\EXE\CLASS\Drivers
+move JUnit/*.class ..\EXE\CLASS\JUnit
