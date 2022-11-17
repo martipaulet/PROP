def1666cde9e14a4263abc6c646c177d62289218
joelf <joel.fernandez.abad@estudiantat.upc.edu>
2022-11-17 11:43:32 +0100

    ctrlpresentacio inici

---
+++ FONTS/Controladors/CtrlPresentacio.java
@@ -0,0 +1,15 @@
+package Controladors;
+import  java.util.*;
+
+public class CtrlPresentacio {
+    private CtrlDomini cd;
+
+    public CtrlPresentacio() {
+        cd = new CtrlDomini();
+        inicialitzarVistes();
+    }
+
+    public void inicialitzarVistes() {
+
+    }
+}
