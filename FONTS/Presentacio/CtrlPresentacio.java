5629741502c99f4bdb192a5548b85f1a110d1e14
albert.cardona.zaera <albert.cardona.zaera@estudiantat.upc.edu>
2022-11-24 11:35:30 +0100

    Canvis presentacio

---
+++ FONTS/Presentacio/CtrlPresentacio.java
@@ -0,0 +1,17 @@
+package Presentacio;
+import Domini.Controladors.CtrlDomini;
+import  java.util.*;
+
+
+public class CtrlPresentacio {
+    private CtrlDomini cd;
+
+    public CtrlPresentacio() {
+        cd = new CtrlDomini();
+        inicialitzarVistes();
+    }
+
+    public void inicialitzarVistes() {
+
+    }
+}
