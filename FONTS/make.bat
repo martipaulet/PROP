4a45a414e63ce0a2c121d1da092a547f9dbf04b9
albert.cardona.zaera <albert.cardona.zaera@estudiantat.upc.edu>
2022-11-24 10:46:38 +0100

    Canvi make

--- FONTS/make.bat
+++ FONTS/make.bat
@@ -1,10 +1,10 @@
-javac Domini/*.java
-javac Domini/*.java Controladors/*.java
-javac -cp lib/junit-4.13.1.jar Domini/*.java JUnit/*.java
-javac Domini/*.java Controladors/*.java Drivers/*.java
-jar cvfe DriverCtrlDomini.jar Drivers/DriverCtrlDomini Domini/*.class Controladors/*.class Drivers/DriverCtrlDomini.class
-move Domini\*.class ..\EXE\CLASS\
-move Controladors\*class ..\EXE\CLASS\
+javac Domini/Model/*.java
+javac Domini/Model/*.java Domini/Controladors/*.java
+javac -cp lib/junit-4.13.1.jar Domini/Model/*.java JUnit/*.java
+javac Domini/Model/*.java Domini/Controladors/*.java Drivers/*.java
+jar cvfe DriverCtrlDomini.jar Drivers/DriverCtrlDomini Domini/Model/*.class Domini/Controladors/*.class Drivers/DriverCtrlDomini.class
+move Domini\Model\*.class ..\EXE\CLASS\
+move Domini\Controladors\*class ..\EXE\CLASS\
 move Drivers\*.class ..\EXE\CLASS\
 move JUnit\*.class ..\EXE\CLASS\
 timeout 5
