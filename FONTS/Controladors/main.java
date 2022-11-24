934596ba88567b19fdccc8161fb4063b4ed79020
Oriol Ologaray <oriol.ologaray@estudiantat.upc.edu>
2022-11-24 10:53:08 +0100

    JavaFX

---
+++ FONTS/Controladors/main.java
@@ -0,0 +1,24 @@
+package Controladors;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+public class main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource(""));
+        Scene s = new Scene(root);
+        primaryStage.setScene(s);
+        primaryStage.show();
+    }
+}
